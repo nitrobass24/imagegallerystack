c3f6d12498bea61922bd20acc2b23497fd45ee56
nitrobass24 <nitrobass24@gmail.com>
2023-03-06 13:19:38 -0600

    Create gatsby-config.js

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  siteMetadata: {
+    title: "My Gatsby Site",
+    description: "A description of my site",
+    author: "Your Name",
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+  ],
+};
