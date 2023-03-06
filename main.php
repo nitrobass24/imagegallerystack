05cfb0bca6014614adfc42ef1ae30d9e92a35e1e
nitrobass24 <nitrobass24@gmail.com>
2023-03-06 12:52:31 -0600

    Create main.php

---
+++ main.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>My JAMstack Website</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <header>
+      <h1>Welcome to my JAMstack website</h1>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/blog">Blog</a></li>
+          <li><a href="/r/images">Images</a></li> <!-- New link added here -->
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <h2>Featured Article</h2>
+      <article>
+        <h3>Article Title</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </article>
+    </main>
+    <footer>
+      <p>&copy; 2023 My JAMstack Website</p>
+    </footer>
+  </body>
+</html>
