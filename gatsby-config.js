952bbbf239dda3b55c55dd4ab12f101408a56c94
nitrobass24 <nitrobass24@gmail.com>
2023-03-11 17:28:40 -0600

    udpated deps

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,8 +9,6 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-styled-components",
-    "gatsby-plugin-image",
-    "gatsby-plugin-sitemap",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     `gatsby-plugin-sass`,
