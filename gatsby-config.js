33901d9d2980141b45c1ea74b1d917d4ea79d138
nitrobass24 <nitrobass24@gmail.com>
2023-03-11 15:27:36 -0600

    Initial upload

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,26 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
 module.exports = {
   siteMetadata: {
-    title: "My Gatsby Site",
-    description: "A description of my site",
-    author: "Your Name",
+    title: `Gallery`,
+    siteUrl: `https://www.yourdomain.tld`,
+    author: `Mbk`,
   },
   plugins: [
+    "gatsby-plugin-styled-components",
     "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
+    `gatsby-plugin-sass`,
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
   ],
 };
