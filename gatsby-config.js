d62959f23d10ed1ccd5a32f509269560dd001e9e
nitrobass24 <nitrobass24@gmail.com>
2023-03-12 20:01:36 -0500

    Add gatsby-transformer-remark plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
     "gatsby-plugin-styled-components",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
+    "gatsby-transformer-remark",
     `gatsby-plugin-sass`,
     {
       resolve: "gatsby-source-filesystem",
