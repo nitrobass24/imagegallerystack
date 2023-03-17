1a07e1e5d912e3efdff61cfa0e257a1dcf7705f5
nitrobass24 <nitrobass24@gmail.com>
2023-03-16 21:14:59 -0500

    Update Gallery.js

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -27,14 +27,14 @@ const Container = styled.div`
   break-inside: avoid-column; /* prevent items from breaking across columns */
   //margin-top: 100px;
   //margin-bottom: 100px;
-  //margin-bottom: 18px; /* add margin to the bottom of each item to create spacing */
+  margin-bottom: 18px; /* add margin to the bottom of each item to create spacing */
   box-sizing: border-box;
   box-shadow: 1px 1px 10px #444;
   width: 100%;
   height: auto;
-  padding: 0; //10px 5px;
-  margin: 0 5px;
-  border: 10px solid transparent;
+  //padding: 0; //10px 5px;
+  //margin: 0 5px;
+  //border: 10px solid transparent;
   //margin-right: 9px;
   //margin-left: 9px;
 }
