f93bf67d53be4b2ad9bb06df6ceb49ab32c7cc42
nitrobass24 <nitrobass24@gmail.com>
2023-03-16 20:40:08 -0500

    Update Gallery.js

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -24,8 +24,8 @@ const Container = styled.div`
 
 .insta {
   break-inside: avoid-column; /* prevent items from breaking across columns */
-  margin-top: 10px;
-  margin-bottom: 10px;
+  margin-top: 100px;
+  margin-bottom: 100px;
   //margin-bottom: 18px; /* add margin to the bottom of each item to create spacing */
   box-sizing: border-box;
   box-shadow: 1px 1px 10px #444;
